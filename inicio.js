9c5bbcc869ee4e0f72c73425ab73c0b9a2a36035
levzr <levisalomonc@gmail.com>
2025-11-04 18:40:53 -0600

    Cambio a 3000

--- inicio.js
+++ inicio.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // ======================================================
 // 🧠 PedidosHN - Servidor Express + EJS
-// Autor: Vidal Cisneros
-// Descripción: Backend principal de pedidos tipo UberEats
-// Puerto: 4000
+// Descripción: Backend principal de pedidos o entregas para restaurantes
+// Puerto: 3000
 // ======================================================
 
 import express from "express";
@@ -17,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // ======================================================
 // 📁 RUTAS DE ARCHIVOS
